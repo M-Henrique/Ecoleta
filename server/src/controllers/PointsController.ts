8bee27f58d4bbbe45020ad686b2030ca24ca9ac2
M-Henrique <48817472+M-Henrique@users.noreply.github.com>
2020-08-13 20:32:09 -0300

    Omit IP

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -21,7 +21,7 @@ class PointsController {
       const serializedPoints = points.map((point) => {
          return {
             ...point,
-            image_url: `http://192.168.1.109:3333/uploads/${point.image}`,
+            image_url: `{yourIpHere}/uploads/${point.image}`,
          };
       });
 
